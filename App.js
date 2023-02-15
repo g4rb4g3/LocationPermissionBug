9dcef39071edd02d3d6f0df8bd86f73521619576
g4rb4g3 <g4rb4g3@gmail.com>
2023-02-15 13:12:46 +0100

    location permission bug example

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    Location.requestForegroundPermissionsAsync().then((result) => {
+      setText('requestForegroundPermissionsAsync: ' + JSON.stringify(result));
+      Location.requestPermissionsAsync().then((result) => {
+        setText((text) => text + '\n\nrequestPermissionsAsync: ' + JSON.stringify(result));
+      });
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
